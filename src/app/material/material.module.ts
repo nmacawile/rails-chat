e3f215dae62c4ec1b421f443236dff47106bc28f
Mac <nmacawile@gmail.com>
2019-04-11 12:11:17 +0800

    Add Angular Material

---
+++ src/app/material/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule, MatIconModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [MatToolbarModule, MatIconModule],
+})
+export class MaterialModule {}
