3ee6a6d93a13b34b3aab4248606cac70da089cfb
Mac <nmacawile@gmail.com>
2019-04-28 15:20:40 +0800

    Import ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,26 @@ import { ScrollDispatchModule } from '@angular/cdk/scrolling';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { MaterialModule } from './material/material.module';
 import { ChatComponent } from './chat/chat.component';
 import { MessageComponent } from './message/message.component';
 import { ChatListComponent } from './chat-list/chat-list.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, ChatComponent, MessageComponent, ChatListComponent,],
+  declarations: [
+    AppComponent,
+    ChatComponent,
+    MessageComponent,
+    ChatListComponent,
+    LoginComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
     ScrollDispatchModule,
