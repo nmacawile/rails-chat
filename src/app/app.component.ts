4121ffc5f650a29149115c238a99b0c7d2c082bd
Normar Macawile <nmacawile@gmail.com>
2019-07-10 23:23:35 +0800

    Improve layout for larger screens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, OnDestroy } from '@angular/core';
 import { routeAnimation } from './animations';
 import { CoreService } from './core.service';
 import { Router } from '@angular/router';
@@ -6,6 +6,8 @@ import { userSetter } from './token-store';
 import { User } from './user';
 import { Store, select } from '@ngrx/store';
 import { CableService } from './cable.service';
+import { MediaObserver } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -14,20 +16,38 @@ import { CableService } from './cable.service';
   animations: [routeAnimation],
   encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
   visible: boolean;
+  mediaSubscription: Subscription;
+  smallScreen: boolean;
 
   constructor(
     public coreService: CoreService,
     private cableService: CableService,
     private router: Router,
     private store: Store<{ user: User }>,
+    public media: MediaObserver,
   ) {
     this.store.pipe(select('auth')).subscribe((user: User) => {
       this.visible = user && user.visible;
       if (user) this.cableService.connect();
       else this.cableService.disconnect();
     });
+
+    this.mediaSubscription = media.media$.subscribe(
+      mediaChange => {
+        if (
+          mediaChange.mqAlias == 'xs' ||
+          mediaChange.mqAlias == 'sm')
+          this.smallScreen = true;
+        else
+          this.smallScreen = false;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.mediaSubscription.unsubscribe();
   }
 
   logOut() {
